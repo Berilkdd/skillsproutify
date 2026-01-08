057ea0571fcc6ff19b30ee0b7f91d870d55c5dba
Berilkdd <brlkrdnz@hotmail.com>
2026-01-08 21:56:14 +0000

    readies code for deploy

---
+++ skillsproutify/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for skillsproutify project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'skillsproutify.settings')
+
+application = get_wsgi_application()
