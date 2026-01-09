ce83614a5445234e10560f97ed3cf98d4fe4df0d
Berilkdd <brlkrdnz@hotmail.com>
2026-01-09 10:26:51 +0000

    Set up core URLs and include in project URLs

--- skillsproutify/urls.py
+++ skillsproutify/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import my_core
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('core/', my_core, name='core'),
+    path('core/', my_core, include('core.urls')),
 ]
+
+
