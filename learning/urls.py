90c0c80c40308547f9127d6d3949cfe09792a4e2
Berilkdd <brlkrdnz@hotmail.com>
2026-05-30 22:59:52 +0100

    Build learning app flow and backend functionality

---
+++ learning/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("first-role/", views.first_role, name="first_role"),
+    path("roles/", views.selected_roles, name="selected_roles"),
+]
--- learning/views.py
+++ learning/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import JobRole
+from .forms import JobRoleForm
 
-# Create your views here.
+def first_role(request):
+    return render(request, "learning/first_role.html")
+
+
+def selected_roles(request):
+    roles = JobRole.objects.all()  
+    form = JobRoleForm()            
+
+    if request.method == "POST":
+        form = JobRoleForm(data=request.POST)
+        if JobRoleForm.is_valid():
+            form.save()
+            return redirect('selected_roles')  
+
+
+    return render(request, "learning/selected_roles.html", 
+                  {"roles": roles,
+                   "form": form,}
+    )
