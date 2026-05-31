00372f123501a32a9fbeba8828f5f4db83b58a11
Berilkdd <brlkrdnz@hotmail.com>
2026-05-31 19:37:50 +0100

    Learning models and views updated for job roles and welcome page flow

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('welcome/', views.welcome, name='welcome'),
+    path('', views.home, name='home'),    
 ]
--- learning/views.py
+++ learning/views.py
@@ -5,19 +5,39 @@ from .forms import JobRoleForm
 def first_role(request):
     return render(request, "learning/first_role.html")
 
+def welcome(request):
+    roles = JobRole.objects.filter(user=request.user)
+    form = JobRoleForm()
+    
+    if request.method == "POST":
+        form = JobRoleForm(request.POST)
+        if form.is_valid():
+            job_role = form.save(commit=False)
+            job_role.user = request.user
+            job_role.save()
+            return redirect('selected_roles')          
+    return render(
+        request,
+        "learning/welcome.html",
+        {"roles": roles, "form": form}
+    )
 
 def selected_roles(request):
-    roles = JobRole.objects.all()  
+    roles = JobRole.objects.filter(user=request.user)  
     form = JobRoleForm()            
 
     if request.method == "POST":
-        form = JobRoleForm(data=request.POST)
-        if JobRoleForm.is_valid():
-            form.save()
-            return redirect('selected_roles')  
-
-
-    return render(request, "learning/selected_roles.html", 
-                  {"roles": roles,
-                   "form": form,}
+        form = JobRoleForm(request.POST)
+        if form.is_valid():
+            job_role = form.save(commit=False)
+            job_role.user = request.user
+            job_role.save()
+            return redirect('selected_roles')   
+    return render(
+        request, 
+        "learning/roles.html", 
+        {"roles": roles, "form": form}
     )
+
+
+    
