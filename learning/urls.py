bd3396c1a030df32569f9aa5e4490b9427d91b04
Berilkdd <brlkrdnz@hotmail.com>
2026-05-31 19:36:28 +0100

    Move and redesign welcome page

--- learning/urls.py
+++ learning/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("first-role/", views.first_role, name="first_role"),
     path("roles/", views.selected_roles, name="selected_roles"),
+    path('welcome/', views.welcome, name='welcome'),
 ]
