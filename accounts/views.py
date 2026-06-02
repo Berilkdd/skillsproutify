7c7f1ee880b9d01eab2d48186ebf74263635605f
Berilkdd <brlkrdnz@hotmail.com>
2026-06-02 10:01:05 +0100

    added manual authentication block

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
 from learning.models import JobRole
 
 def home(request):
@@ -12,6 +12,12 @@ def login_redirect(request):
     # The note confirms account has confirmed from mail link      
     # This if block customized with guidance from Google Ai
     if request.session.get('account_email_confirmed'):
+        #Prevents timing problem, without this block the user gets welcome page with AnonymousUser Error
+        #This block confirms login operation completes manually if needed before welcome page rendered
+        if not request.user.is_authenticated:
+            user = request.user
+            user.backend = 'allauth.account.auth_backends.AuthenticationBackend'
+            login(request, user)
         # Session information has to be cleared to prevent existing user to come here
         # so this part is only for a new user comes from sign-in page and email confirmation
         request.session.pop('account_email_confirmed', None)
