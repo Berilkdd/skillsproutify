b24f0832414aa880e9cca1f56d0967aa35c55b6f
Berilkdd <brlkrdnz@hotmail.com>
2026-06-04 12:26:20 +0100

    Implement dynamic welcome page messages for different user stories

--- learning/views.py
+++ learning/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from allauth.account.signals import email_confirmed
+from django.dispatch import receiver
 from .models import JobRole
 from .forms import JobRoleForm
 
@@ -8,6 +10,8 @@ from .forms import JobRoleForm
 def welcome(request):
     print(request.user)
     print(request.user.is_authenticated)
+
+    is_from_email = request.session.get('is_from_email', False)
 
     roles = JobRole.objects.filter(user=request.user)
     form = JobRoleForm()
@@ -19,12 +23,15 @@ def welcome(request):
         if title_data:
             job_role = JobRole(user=request.user, title=title_data)
             job_role.save()
-            return redirect('selected_roles')          
-    return render(
+            return redirect('selected_roles')        
+    response = render(
         request,
         "learning/welcome.html",
-        {"roles": roles, "form": form}
+        {"roles": roles, "form": form, "is_from_email": is_from_email}
     )
+    if 'is_from_email' in request.session:
+        del request.session['is_from_email']
+    return response
 
 @login_required
 def selected_roles(request):
@@ -48,14 +55,14 @@ def selected_roles(request):
 
 @login_required
 def delete_job_role(request, role_id):
-    """
-    Kullanıcının seçtiği rolü güvenli bir şekilde siler.
-    """
-    # Sadece giriş yapmış kullanıcının kendi rolünü bul (Güvenlik zırhı)
+   
     job_role = get_object_or_404(JobRole, id=role_id, user=request.user)
     
     if request.method == "POST":
         job_role.delete()
         
-    # Rol silindikten sonra durum kontrolü için yönlendiriyoruz
     return redirect('/login-redirect/')
+
+@receiver(email_confirmed)
+def set_email_verified_session(request, email_address, **kwargs):
+        request.session['is_from_email'] = True
