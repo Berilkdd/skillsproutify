2746c5a0e4436e36b0d5aea165f976c787fef7bc
Berilkdd <brlkrdnz@hotmail.com>
2026-06-07 14:14:39 +0100

    Add resources template with url path and fix the delete button functionality and sytling on selected role page

--- learning/urls.py
+++ learning/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("roles/", views.selected_roles, name='selected_roles'),
     path('welcome/', views.welcome, name='welcome'),
-    path('roles/delete/<int:role_id>/', views.delete_job_role, name='delete_job_role'),
+    path('roles/<int:role_id>/resources/', views.role_resources, name='role_resources'),
+    path('roles/delete/<int:role_id>/', views.delete_job_role, name='delete_job_role'),    
 ]
