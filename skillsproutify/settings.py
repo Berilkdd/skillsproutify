7293766d04571ad0480d6540d4a26c9a2fcf73fc
Berilkdd <brlkrdnz@hotmail.com>
2026-06-07 12:11:07 +0100

    Fix syntax error in INSTALLED_APPS

--- skillsproutify/settings.py
+++ skillsproutify/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'accounts',git
+    'accounts',
     'learning',
     'widget_tweaks',
     'allauth',
