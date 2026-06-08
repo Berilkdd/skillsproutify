d8956ecd16d374aa1299624807ae19eb9ead7910
Berilkdd <brlkrdnz@hotmail.com>
2026-06-08 17:42:35 +0100

    Fix PEP8 styling in learning admin

--- learning/admin.py
+++ learning/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from .models import JobRole, Resource, ResourceItem
 
+
 @admin.register(JobRole)
 class JobRoleAdmin(admin.ModelAdmin):
     list_display = ('user', 'title')
     search_fields = ('user__username', 'title')
 
+
 @admin.register(Resource)
 class ResourceAdmin(admin.ModelAdmin):
     list_display = ('job_role', 'name')
     search_fields = ('job_role__title', 'name')
+
 
 @admin.register(ResourceItem)
 class ResourceItemAdmin(admin.ModelAdmin):
