095127bd6cc0ae4cc8469321535e6d02d3f5436a
Berilkdd <brlkrdnz@hotmail.com>
2026-06-08 19:09:02 +0100

    Complete Python validation section on README

--- skillsproutify/urls.py
+++ skillsproutify/urls.py
@@ -19,9 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("accounts/", include('allauth.urls')),
-    path('admin/', admin.site.urls),  
-    path('', include('accounts.urls')),    
+    path('admin/', admin.site.urls),
+    path('', include('accounts.urls')),
     path('learning/', include('learning.urls'))
 ]
-
-
