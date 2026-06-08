293d0d02ea414003fb5548a92959976cadc2b13b
Berilkdd <brlkrdnz@hotmail.com>
2026-06-08 17:39:25 +0100

    Fix PEP8 styling in learning urls

--- learning/urls.py
+++ learning/urls.py
@@ -2,10 +2,22 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('welcome/', views.welcome, name='welcome'),   
+    path('welcome/', views.welcome, name='welcome'),
     path("roles/", views.selected_roles, name='selected_roles'),
     path('<int:role_id>/', views.role_resources, name='role_resources'),
-    path('items/<int:resource_id>/', views.resource_items, name='resource_items'),
-    path('learning-delete/<str:item_type>/<int:item_id>/', views.delete_item, name='delete_item'), 
-    path('items/<int:item_id>/toggle/', views.toggle_item_status, name='toggle_item_status'),
+    path(
+        'items/<int:resource_id>/',
+        views.resource_items,
+        name='resource_items'
+    ),
+    path(
+        'learning-delete/<str:item_type>/<int:item_id>/',
+        views.delete_item,
+        name='delete_item'
+    ),
+    path(
+        'items/<int:item_id>/toggle/',
+        views.toggle_item_status,
+        name='toggle_item_status'
+    ),
 ]
