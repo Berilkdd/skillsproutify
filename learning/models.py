620f6a272af5079c44a66c4f15397f7be8cc34b4
Berilkdd <brlkrdnz@hotmail.com>
2026-06-08 17:17:16 +0100

    Fix PEP8 styling in learning models

--- learning/models.py
+++ learning/models.py
@@ -2,18 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
+
 class JobRole(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
-    
+
     def clean(self):
-        #Check the quantity before save
+        # Check the quantity before save
         if self.user.jobrole_set.count() >= 5 and not self.pk:
             raise ValidationError("You can only have up to 5 roles.")
-        
+
+
 class Resource(models.Model):
     job_role = models.ForeignKey(JobRole, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -22,9 +24,10 @@ class Resource(models.Model):
         return self.name
 
     def clean(self):
-        #Check the quantity before save
+        # Check the quantity before save
         if self.job_role.resource_set.count() >= 5 and not self.pk:
             raise ValidationError("You can only have up to 5 resources.")
+
 
 class ResourceItem(models.Model):
     resource = models.ForeignKey(Resource, on_delete=models.CASCADE)
