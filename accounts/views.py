005c42497635359174a10a8adef45115b09a8077
Berilkdd <brlkrdnz@hotmail.com>
2026-06-08 17:07:06 +0100

    Fix PEP8 styling in accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from learning.models import JobRole
 
+
 def home(request):
     return render(request, 'home.html')
+
 
 @login_required
 def login_redirect(request):
     if request.user.jobrole_set.exists():
-            # If they alredy have some roles selected
-            return redirect('selected_roles')
-        # If they haven't got any role yet
+        # If they already have some roles selected
+        return redirect('selected_roles')
+    # If they haven't got any role yet
     return redirect('welcome')
-
