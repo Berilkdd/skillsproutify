e0501948ce01e7efbfc41835ca6f148c565af736
Berilkdd <brlkrdnz@hotmail.com>
2026-07-13 22:41:31 +0100

    Readme updates

--- learning/urls.py
+++ learning/urls.py
@@ -17,10 +17,10 @@ urlpatterns = [
     ),
 
     path(
-    "update/<str:item_type>/<int:item_id>/",
-    views.update_item,
-    name="update_item",
-    ),
+        "update/<str:item_type>/<int:item_id>/",
+        views.update_item,
+        name="update_item",
+        ),
 
     path(
         'items/<int:item_id>/toggle/',
